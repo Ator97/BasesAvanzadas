315a2c53293a15a1bd7537e3488450e83f444b7b
Ator97 <oscargutcas9705@gmail.com>
2019-12-07 21:38:11 -0600

    Carga inicial

--- PAIS.sql
+++
@@ -1,5 +0,0 @@
-insert into PAIS (PAIS_ID, CLAVE, NOMBRE) values (1, 'US', 'United States');
-insert into PAIS (PAIS_ID, CLAVE, NOMBRE) values (2, 'MX', 'Mexico');
-insert into PAIS (PAIS_ID, CLAVE, NOMBRE) values (3, 'CN', 'China');
-insert into PAIS (PAIS_ID, CLAVE, NOMBRE) values (4, 'GR', 'Greece');
-insert into PAIS (PAIS_ID, CLAVE, NOMBRE) values (5, 'RU', 'Russia');
